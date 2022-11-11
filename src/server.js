d7a186f8847a60387a65558d930eb7d31a2b4556
KietHo1 <kiethostartup1999@gmail.com>
2022-11-11 10:22:20 +0700

    video6

--- src/server.js
+++ src/server.js
@@ -1,13 +1,16 @@
-const express = require("express");
+import express from "express";
+import configViewEngine from "./configs/viewEngine";
 
 const app = express();
 const port = 8080;
 
+configViewEngine(app);
+
 app.get("/", (req, res) => {
-  res.send("Hello World! vs KietHo1");
+  res.render("test/index.ejs");
 });
 app.get("/about", (req, res) => {
-  res.send(`I'm Kiet!`);
+  res.send(`I'm Eric!`);
 });
 
 app.listen(port, () => {
