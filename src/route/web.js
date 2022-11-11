3a7e67291e729eb176baa123ed323d25480d336c
KietHo1 <kiethostartup1999@gmail.com>
2022-11-11 21:17:31 +0700

    video 11 connection pool view detail user

--- src/route/web.js
+++ src/route/web.js
@@ -1,13 +1,14 @@
 import express from "express";
 import homeController from "../controller/homeController";
 let router = express.Router();
-//web quản lý các route
+
 const initWebRoute = (app) => {
   router.get("/", homeController.getHomepage);
+  router.get("/detail/user/:id", homeController.getDetailPage);
   router.get("/about", (req, res) => {
     res.send(`I'm Eric!`);
   });
-  //Thêm tiền tố vào phía trước các route - vd: /api/v1/
+
   return app.use("/", router);
 };
 
