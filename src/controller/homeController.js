7358f4f287e422cf3faa93a8142e7107eaf75bbe
KietHo1 <kiethostartup1999@gmail.com>
2022-11-12 21:49:00 +0700

    video15 part1 upload single file

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,4 +1,5 @@
 import pool from "../configs/connectDB";
+import multer from "multer";
 
 let getHomepage = async (req, res) => {
   const [rows, fields] = await pool.execute("SELECT * FROM users");
@@ -46,6 +47,36 @@ let postUpdateUser = async (req, res) => {
   return res.redirect("/");
 };
 
+let getUploadFilePage = async (req, res) => {
+  return res.render("uploadFile.ejs");
+};
+
+const upload = multer().single("profile_pic");
+
+let handleUploadFile = async (req, res) => {
+  // 'profile_pic' is the name of our file input field in the HTML form
+
+  upload(req, res, function (err) {
+    // req.file contains information of uploaded file
+    // req.body contains information of text fields, if there were any
+
+    if (req.fileValidationError) {
+      return res.send(req.fileValidationError);
+    } else if (!req.file) {
+      return res.send("Please select an image to upload");
+    } else if (err instanceof multer.MulterError) {
+      return res.send(err);
+    } else if (err) {
+      return res.send(err);
+    }
+
+    // Display uploaded image for user validation
+    res.send(
+      `You have uploaded this image: <hr/><img src="/image/${req.file.filename}" width="500"><hr /><a href="/upload">Upload another image</a>`
+    );
+  });
+};
+
 module.exports = {
   getHomepage,
   getDetailPage,
@@ -53,4 +84,6 @@ module.exports = {
   deleteUser,
   getEditPage,
   postUpdateUser,
+  getUploadFilePage,
+  handleUploadFile,
 };
