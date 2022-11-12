0ea463dd14b9d6a5d6feeb20216b043c1cf5c1ef
KietHo1 <kiethostartup1999@gmail.com>
2022-11-12 10:04:07 +0700

    video12 create new user

--- src/route/web.js
+++ src/route/web.js
@@ -5,6 +5,7 @@ let router = express.Router();
 const initWebRoute = (app) => {
   router.get("/", homeController.getHomepage);
   router.get("/detail/user/:id", homeController.getDetailPage);
+  router.post("/create-new-user", homeController.createNewUser);
   router.get("/about", (req, res) => {
     res.send(`I'm Eric!`);
   });
