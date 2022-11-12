76d404aa2f93e88d82cffde3feacd8c9ca05638f
KietHo1 <kiethostartup1999@gmail.com>
2022-11-12 11:20:25 +0700

    video 13 finish CRUD

--- src/route/web.js
+++ src/route/web.js
@@ -6,6 +6,11 @@ const initWebRoute = (app) => {
   router.get("/", homeController.getHomepage);
   router.get("/detail/user/:id", homeController.getDetailPage);
   router.post("/create-new-user", homeController.createNewUser);
+
+  router.post("/delete-user", homeController.deleteUser);
+  router.get("/edit-user/:id", homeController.getEditPage);
+  router.post("/update-user", homeController.postUpdateUser);
+
   router.get("/about", (req, res) => {
     res.send(`I'm Eric!`);
   });
